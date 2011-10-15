de387d213f861d824bcbe59329edde34e32c04a9
mces89 <shyang.ok@gmail.com>
2011-10-15 20:23:13 +0800

    test

---
+++ algorithm/src/algorithm.cpp
@@ -0,0 +1,15 @@
+//============================================================================
+// Name        : algorithm.cpp
+// Author      : mces89
+// Version     :
+// Copyright   : Your copyright notice
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	cout << "!!!Hello World!!!" << endl; // prints !!!Hello World!!!
+	return 0;
+}
